c4de91a80b332eaae8f52cc4db42df9ac6720bb1
David-T7 <tesfayedawit524@gmail.com>
2022-10-26 05:09:49 -0700

    intial commit

---
+++ app/src/main/java/com/example/gittutorial/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gittutorial;
+
+import static java.sql.DriverManager.println;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        println("this is my first message");
+    }
+}
