5b2bae06dbd6b5771f4a3c402ec0075f7270ace5
David-T7 <tesfayedawit524@gmail.com>
2022-10-26 05:11:19 -0700

    commit after the second message

--- app/src/main/java/com/example/gittutorial/MainActivity.java
+++ app/src/main/java/com/example/gittutorial/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         println("this is my first message");
+        println("this is my second message");
     }
 }
